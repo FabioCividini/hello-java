c19ad5b096d249f876bddbc23477b3834aa15794
FabioCividini <fabiocividini22@gmail.com>
2021-11-12 14:38:59 +0100

    src/org/generation/italy/StampaSaluto.java

---
+++ src/org/generation/italy/StampaSaluto.java
@@ -0,0 +1,14 @@
+package org.generation.italy;
+
+public class StampaSaluto {
+	//questo è il metodo che esegue la classe
+	public static void main(String[] args) {
+		/*
+		 * io sono un commento funzionale per più righe
+		 */
+		//
+		System.out.println("Ciao JAVA ciao mondo");
+
+	}
+
+}
